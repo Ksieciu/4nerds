3c1895bd33dcb47775e8e24b847f3c192e067632
Ksieciu <antelius14@gmail.com>
2019-06-02 21:33:13 +0200

    navbar ver 1
    Dodaje próbny navbar

---
+++ navscript.js
@@ -0,0 +1,20 @@
+
+window.onscroll = function() {sticking()};
+
+const navbar = document.querySelector(".stick-nav");
+let stick = navbar.offsetTop;
+
+
+function sticking(){
+    console.log("sitckuje!");
+    if(window.pageYOffset >= stick){
+        navbar.classList.add("stick");
+    } else {
+        navbar.classList.remove("stick");
+    }
+};
+
+
+function height(){
+    return navbar.offsetHeight;
+};
